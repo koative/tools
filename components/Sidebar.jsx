fc86a0a877adf821a32e53fc7d33520bcbc05ab1
Koray Güler <us.koray.guler@gmail.com>
2021-08-22 02:55:00 +0300

    Create Sidebar

---
+++ components/Sidebar.jsx
@@ -0,0 +1,9 @@
+const Sidebar = () => {
+  return (
+    <aside className='p-6 sticky top-0 bg-white shadow-lg  w-80 flex-shrink-0'>
+      sidebar
+    </aside>
+  );
+};
+
+export default Sidebar;
