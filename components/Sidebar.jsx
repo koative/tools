faf68e60bb18b06231f3b9beea9f137aba74099d
Koray Güler <us.koray.guler@gmail.com>
2021-08-22 05:19:35 +0300

    test commit

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,10 @@
+import Link from 'next/link';
+
 const Sidebar = () => {
   return (
     <aside className='p-6 sticky top-0 bg-white shadow-lg  w-80 flex-shrink-0'>
-      sidebar
+      <Link href='/'>Go Home</Link>
+      <Link href='/sample'>Go Sample</Link>
     </aside>
   );
 };
